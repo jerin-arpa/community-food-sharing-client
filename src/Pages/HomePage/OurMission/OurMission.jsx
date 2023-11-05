0dcfdf00c1be69ebe8cae17edeb8f40a404fe929
jerin-arpa <ismatjerrin3@gmail.com>
2023-11-05 17:18:43 +0600

    Install AOS package and apply it into our mission section

--- src/Pages/HomePage/OurMission/OurMission.jsx
+++ src/Pages/HomePage/OurMission/OurMission.jsx
@@ -3,10 +3,17 @@ import { FaBowlFood, FaPersonShelter } from "react-icons/fa6";
 import { FaGripfire, FaTshirt } from "react-icons/fa";
 import { BsBookHalf, BsFillHeartPulseFill } from "react-icons/bs";
 import { IoWater } from "react-icons/io5";
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 
 
 const OurMission = () => {
+
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
     return (
         <div className="container mx-auto px-5 py-10 pb-28">
             <div>
@@ -35,68 +42,68 @@ const OurMission = () => {
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 mt-20">
                 <div className="border border-[#23ad0e] p-10 relative">
-                    <div className="flex justify-center absolute -top-8 left-48">
+                    <div data-aos="fade-right" data-aos-duration="3000" className="flex justify-center absolute -top-8 left-48">
                         <FaBowlFood className="text-7xl bg-[#23ad0e] text-white p-2"></FaBowlFood>
                     </div>
 
-                    <h2 className="text-2xl font-bold text-center my-5 mt-8">Charity For Food</h2>
+                    <h2 data-aos="fade-right" data-aos-duration="3000" className="text-2xl font-bold text-center my-5 mt-8">Charity For Food</h2>
 
-                    <p className="text-center">Focused on eradicating hunger, this charity strives to provide nutritious meals to underprivileged individuals and families, ensuring no one goes to bed hungry.</p>
+                    <p data-aos="fade-right" data-aos-duration="3000" className="text-center">Focused on eradicating hunger, this charity strives to provide nutritious meals to underprivileged individuals and families, ensuring no one goes to bed hungry.</p>
                 </div>
 
 
                 <div className="border border-[#23ad0e] p-10 relative">
-                    <div className="flex justify-center absolute -top-8 left-48">
+                    <div className="flex justify-center absolute -top-8 left-48" data-aos="fade-down" data-aos-duration="3000">
                         <FaTshirt className="text-7xl bg-[#23ad0e] text-white p-2"></FaTshirt>
                     </div>
 
-                    <h2 className="text-2xl font-bold text-center my-5 mt-8">Charity For Cloth</h2>
+                    <h2 data-aos="fade-down" data-aos-duration="3000" className="text-2xl font-bold text-center my-5 mt-8">Charity For Cloth</h2>
 
-                    <p className="text-center">This charity's mission is to clothe the less fortunate, offering warmth, dignity, and protection from the elements, fostering self-esteem and comfort.</p>
+                    <p data-aos="fade-down" data-aos-duration="3000" className="text-center">This charity's mission is to clothe the less fortunate, offering warmth, dignity, and protection from the elements, fostering self-esteem and comfort.</p>
                 </div>
 
 
                 <div className="border border-[#23ad0e] p-10 relative">
-                    <div className="flex justify-center absolute -top-8 left-48">
+                    <div data-aos="fade-left" data-aos-duration="3000" className="flex justify-center absolute -top-8 left-48">
                         <BsBookHalf className="text-7xl bg-[#23ad0e] text-white p-2"></BsBookHalf>
                     </div>
 
-                    <h2 className="text-2xl font-bold text-center my-5 mt-8">Charity For Education</h2>
+                    <h2 data-aos="fade-left" data-aos-duration="3000" className="text-2xl font-bold text-center my-5 mt-8">Charity For Education</h2>
 
-                    <p className="text-center">Committed to empowering the next generation, this charity supports educational initiatives that open doors to knowledge and opportunities.</p>
+                    <p data-aos="fade-left" data-aos-duration="3000" className="text-center">Committed to empowering the next generation, this charity supports educational initiatives that open doors to knowledge and opportunities.</p>
                 </div>
 
 
                 <div className="border border-[#23ad0e] p-10 relative">
-                    <div className="flex justify-center absolute -top-8 left-48">
+                    <div data-aos="fade-right" data-aos-duration="3000" className="flex justify-center absolute -top-8 left-48">
                         <BsFillHeartPulseFill className="text-7xl bg-[#23ad0e] text-white p-2"></BsFillHeartPulseFill>
                     </div>
 
-                    <h2 className="text-2xl font-bold text-center my-5 mt-8">Charity For Health</h2>
+                    <h2 data-aos="fade-right" data-aos-duration="3000" className="text-2xl font-bold text-center my-5 mt-8">Charity For Health</h2>
 
-                    <p className="text-center"> Providing healthcare services and resources to those in need, this charity aims to improve physical and mental well-being.</p>
+                    <p data-aos="fade-right" data-aos-duration="3000" className="text-center"> Providing healthcare services and resources to those in need, this charity aims to improve physical and mental well-being.</p>
                 </div>
 
 
                 <div className="border border-[#23ad0e] p-10 relative">
-                    <div className="flex justify-center absolute -top-8 left-48">
+                    <div data-aos="fade-up" data-aos-duration="3000" className="flex justify-center absolute -top-8 left-48">
                         <FaPersonShelter className="text-7xl bg-[#23ad0e] text-white p-2"></FaPersonShelter>
                     </div>
 
-                    <h2 className="text-2xl font-bold text-center my-5 mt-8">Charity For Shelter</h2>
+                    <h2 data-aos="fade-up" data-aos-duration="3000" className="text-2xl font-bold text-center my-5 mt-8">Charity For Shelter</h2>
 
-                    <p className="text-center">Focused on housing the homeless and vulnerable, this charity ensures a safe and  offering a chance for individuals to rebuild their lives.</p>
+                    <p data-aos="fade-up" data-aos-duration="3000" className="text-center">Focused on housing the homeless and vulnerable, this charity ensures a safe and  offering a chance for individuals to rebuild their lives.</p>
                 </div>
 
 
                 <div className="border border-[#23ad0e] p-10 relative">
-                    <div className="flex justify-center absolute -top-8 left-48">
+                    <div data-aos="fade-left" data-aos-duration="3000" className="flex justify-center absolute -top-8 left-48">
                         <IoWater className="text-7xl bg-[#23ad0e] text-white p-2"></IoWater>
                     </div>
 
-                    <h2 className="text-2xl font-bold text-center my-5 mt-8">Charity For Clean Water</h2>
+                    <h2 data-aos="fade-left" data-aos-duration="3000" className="text-2xl font-bold text-center my-5 mt-8">Charity For Clean Water</h2>
 
-                    <p className="text-center">Dedicated to securing access to clean and safe drinking water, improving health and overall living conditions in impoverished regions.</p>
+                    <p data-aos="fade-left" data-aos-duration="3000" className="text-center">Dedicated to securing access to clean and safe drinking water, improving health and overall living conditions in impoverished regions.</p>
                 </div>
             </div>
         </div >
